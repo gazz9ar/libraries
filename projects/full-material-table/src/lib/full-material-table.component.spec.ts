8406dd056c4252f9c3c9ffd3b350d4410f2cbdda
Gaspar Santillan <gazz9ar@gmail.com>
2023-01-04 10:51:38 -0300

    unit test

--- projects/full-material-table/src/lib/full-material-table.component.spec.ts
+++ projects/full-material-table/src/lib/full-material-table.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FullMaterialTableComponent } from './full-material-table.component';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from './material.module';
+import { FullMaterialTableService } from './full-material-table.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FullMaterialTableComponent', () => {
   let component: FullMaterialTableComponent;
@@ -8,7 +12,17 @@ describe('FullMaterialTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FullMaterialTableComponent ]
+      declarations: [ 
+        FullMaterialTableComponent 
+      ],
+      imports: [
+        CommonModule,
+        MaterialModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        FullMaterialTableService
+      ]
     })
     .compileComponents();
 

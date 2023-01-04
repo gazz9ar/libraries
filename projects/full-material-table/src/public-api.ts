4f5cc2ffe4b4a34218456facf1804cb81dd8d0f2
Gaspar Santillan <gazz9ar@gmail.com>
2023-01-04 11:16:51 -0300

    fix files

--- projects/full-material-table/src/public-api.ts
+++ projects/full-material-table/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of full-material-table
  */
 
-export * from './lib/full-material-table.service';
-export * from './lib/full-material-table.component';
 export * from './lib/full-material-table.module';
